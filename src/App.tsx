bb50b9292f89ac4d88410035b1d9c2692802dd26
thlindustries <thlindustries23@gmail.com>
2020-07-30 12:30:17 -0300

    feature: eslint configuration and functional modules

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Lading from './pages/landingPage';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return <Lading />;
+};
+
+export default App;
---
+++ src/pages/landingPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const landingPage: React.FC = () => {
+  return (
+    <Container>
+      <p>Landing page</p>
+    </Container>
+  );
+};
+
+export default landingPage;
