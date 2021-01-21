b8d51e027595f038372ad55c9d519099f5005126
root <thiago.kraetzer@brainweb.com.br>
2021-01-21 14:07:09 -0300

    Fix: fixing

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -6,19 +6,8 @@ import LogoHorizontal from 'assets/SITE/logo_complete.png';
 import { Container, Tabs } from './styles';
 
 const Header: React.FC = () => {
-  // const [isReduced, setIsReduced] = useState(false);
-  // const [heightFromTop, setHeightFromTop] = useState(0);
   const headerRef = useRef<HTMLDivElement>(null);
-
-  // const scrollFunction = (): void => {
-  //   console.log(headerRef.current?.scrollTop);
-  // };
-  // console.log(isReduced);
-
-  // window.onscroll = () => scrollFunction();
-
   return (
-    // <Container ref={headerRef} className={isReduced ? 'on' : ''}>
     <Container ref={headerRef}>
       <img src={LogoHorizontal} alt="Horizontal logo" />
       <Tabs>
